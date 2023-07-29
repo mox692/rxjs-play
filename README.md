e36de3a82cba5b1675d08c626dd873ce7aeb2d5b
mox692 <moymoymox@gmail.com>
2023-07-29 13:00:07 +0900

    Add some example code

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 template: https://github.com/seeschweiler/rxjs-part1/tree/master
 
 tutorial: https://rxjs.dev/guide/overview
+
+# TODO
+* 一定の条件を満たしたら、subscribeをやめたい
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
-import { fromEvent, scan } from 'rxjs';
+import { bufferTime, filter, fromEvent, scan, throttleTime, map } from 'rxjs';
 
-const button = document.getElementById("buttonId")
+const button = document.getElementById("firstButton")
+const multipleOperatorFunctionButton = document.getElementById("multipleOperatorFunction")
+const doubleClick = document.getElementById("doubleClick")
 
+
+// simple counter
 fromEvent(button, 'click')
-    .pipe((scan))
+    .pipe(
+        scan((count: number) => count + 1, 0),
+    )
+    .subscribe((count: number) => 
+        console.log(`count: ${count}`)
+    )
+
+// 偶数だけをprintする
+fromEvent(multipleOperatorFunctionButton, 'click')
+    .pipe(
+        scan((count: number) => count + 1, 0),
+        filter((count: number) => count % 2 === 0)
+    )
+    .subscribe((count: number) => 
+        console.log(`even count: ${count}`)
+    )
+
+// double click checker
+fromEvent(doubleClick, 'click')
+    .pipe(
+        bufferTime(250),
+        map((clicks: Array<Event>) => clicks.length),
+        filter((clickLength: number)=> clickLength >= 2)
+    )
+    .subscribe((_: any) => 
+        console.log(`double click detected!!`)
+    )
