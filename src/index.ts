d4166e5211dcc392d308a2d9eda6021eea317ada
mox692 <moymoymox@gmail.com>
2023-07-29 13:48:04 +0900

    Add more example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { bufferTime, filter, fromEvent, scan, throttleTime, map } from 'rxjs';
+import { bufferTime, filter, fromEvent, scan, map, Observable, Subscriber } from 'rxjs';
 
 const button = document.getElementById("firstButton")
 const multipleOperatorFunctionButton = document.getElementById("multipleOperatorFunction")
 const doubleClick = document.getElementById("doubleClick")
 
+/**
+ * Simple getting started Example
+ */
 
 // simple counter
 fromEvent(button, 'click')
@@ -34,3 +37,32 @@ fromEvent(doubleClick, 'click')
     .subscribe((_: any) => 
         console.log(`double click detected!!`)
     )
+
+/**
+ * Observable Example
+ */
+
+const simpleObservable1 = new Observable((subscriber: Subscriber<number>) => {
+    subscriber.next(0)
+    subscriber.next(1)
+    subscriber.next(2)
+    subscriber.next(3)
+})
+
+simpleObservable1.subscribe((data: number) => {
+    console.log(`data: ${data}`)
+})
+
+const asyncObservable = new Observable((subscriber: Subscriber<Promise<number>>) => {
+    subscriber.next(Promise.resolve(1))
+    subscriber.next(Promise.resolve(2))
+    subscriber.next(Promise.resolve(3))
+    subscriber.next(Promise.resolve(4))
+})
+
+asyncObservable.subscribe((data: Promise<number>) => {
+    data.then((x: number) =>{
+        console.log(`resolved data: ${x}`)
+    })
+    
+})
